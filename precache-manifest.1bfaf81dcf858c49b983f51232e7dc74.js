0802a652a93d1deefc9821b16837ad32e558172e
Scott Nicholson <scott.nicholson@live.co.uk>
2020-10-25 22:18:49 +0000

    Updates

---
+++ precache-manifest.1bfaf81dcf858c49b983f51232e7dc74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22fc77804ee6bec88cbd5fa42a66d1a",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "abd1b5fd43cdf4c66f24",
+    "url": "/rock-paper-scissors/static/css/main.943f8077.chunk.css"
+  },
+  {
+    "revision": "2b8a2e933e5ebb626aa1",
+    "url": "/rock-paper-scissors/static/js/2.cf60442e.chunk.js"
+  },
+  {
+    "revision": "abd1b5fd43cdf4c66f24",
+    "url": "/rock-paper-scissors/static/js/main.ba9a0bff.chunk.js"
+  },
+  {
+    "revision": "626d2f628433c4caf654",
+    "url": "/rock-paper-scissors/static/js/runtime~main.4fb342b9.js"
+  }
+]);
